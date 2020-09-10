af9ac4418fa016cb2889233b09d630ec4685418c
Your Name <you@example.com>
2020-09-10 15:45:46 +0545

    Updates

---
+++ precache-manifest.f685bac9d307c8714565c53322adc148.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4efa2d8afb3d31713d2f0737e96d2c95",
+    "url": "/react-covid19-tracker/index.html"
+  },
+  {
+    "revision": "0310ba90daee5fa557d5",
+    "url": "/react-covid19-tracker/static/css/main.07a70da8.chunk.css"
+  },
+  {
+    "revision": "7dfb7eaa6937b42bb632",
+    "url": "/react-covid19-tracker/static/js/2.4cc5ac7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-covid19-tracker/static/js/2.4cc5ac7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0310ba90daee5fa557d5",
+    "url": "/react-covid19-tracker/static/js/main.9f730e41.chunk.js"
+  },
+  {
+    "revision": "9bdca937a058ecb13beb",
+    "url": "/react-covid19-tracker/static/js/runtime-main.2447f8e0.js"
+  }
+]);
